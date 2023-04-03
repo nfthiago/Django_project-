0361dbd924d79b0aa5a09e80d97ac31b770e59f4
Thiago França <nfthiago@gmail.com>
2023-04-03 21:22:55 +0100

    Configuration admin.py to show search engine, filters and more..

--- movieSearch/admin.py
+++ movieSearch/admin.py
@@ -1,7 +1,34 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Profile)
+#class and method that allows to create and customise models
+class ProfileAdmin(admin.ModelAdmin):
+    #Sort registers by date of creation
+    date_hierarchy = 'created_at'
+    #Shows chosen user attributes (columns)
+    list_display = ('user', 'role', 'birthday',)
+    #Allows us to change empty registers to chose 'message'
+    empty_value_display = '---'
+    #Add link to others attributes listed by "list_display"
+    list_display_links = ('user', 'role',)
+    #Create a filter on screen (right side)
+    list_filter = ('user__is_active',)
+    #To determine which fields will be showned on create user form. 
+    # ***Don't let not null fields out of this.***
+    fields = (
+    'user', ('role', ), 'image', 'birthday', 'addresses',)
+    #To exclude fields to be showned on create user form.
+    exclude = ('favorites', 'specialties' 'created_at', 'updated_at',)
+    #Fields set to be 'readonly' on create and edit user forms.
+    readonly_fields = ('user',)
+    #text field that allows user to search for the chosen fields (parameters).
+    #don't work with integers (must use list_filter=('user__is__active', 'role'))
+    search_fields = ('user__username',)
+
+
+# Register your models here.
+#Class "ProfileAdmin" passed as parameter of register method
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)
@@ -11,4 +38,3 @@ admin.site.register(Rating)
 admin.site.register(Genre)
 admin.site.register(Movie)
 
-# Register your models here.
